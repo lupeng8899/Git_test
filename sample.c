b64d7a0d22dadbb93432e7abd2127c2bb0667408
lupeng8899 <mike.lupeng@hotmail.com>
2016-10-27 21:14:41 +0200

    changed the git_test_2 back

--- sample.c
+++ sample.c
@@ -2,14 +2,12 @@
 int main()
 {
 
-<<<<<<< HEAD
+
 
 printf("this is changed in a different local branch called git_test_2 ");
 printf("This is not changed in the local master branch");
 
-=======
-print("note that this is changed in a local branch");
->>>>>>> master
+
 
 printf("hello world");
   

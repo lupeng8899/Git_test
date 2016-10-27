76d265f325fa1fb446dbb802d17e8e8cb523141d
lupeng8899 <mike.lupeng@hotmail.com>
2016-10-27 20:10:21 +0200

    changed in a local branch

--- sample.c
+++ sample.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 int main()
 {
+
+print("note that this is changed in a local branch");
+
 printf("hello world");
   
   print("added this line, so it is different from the previous master branch");

275dafa53a966490aa980367bdc0c54d64256f54
lupeng8899 <mike.lupeng@hotmail.com>
2016-10-27 21:12:36 +0200

    changed the master branch

--- sample.c
+++ sample.c
@@ -2,14 +2,9 @@
 int main()
 {
 
-<<<<<<< HEAD
 
-printf("this is changed in a different local branch called git_test_2 ");
-printf("This is not changed in the local master branch");
+print("I do not know I did.");
 
-=======
-print("note that this is changed in a local branch");
->>>>>>> master
 
 printf("hello world");
   

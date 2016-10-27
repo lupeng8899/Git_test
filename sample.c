afbee2f7841cc0064829b488b2ee073195620385
lupeng8899 <mike.lupeng@hotmail.com>
2016-10-27 20:23:24 +0200

    This is changed in a different local branch, not master branch

--- sample.c
+++ sample.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 int main()
 {
+
+
+printf("this is changed in a different local branch called git_test_2 ");
+printf("This is not changed in the local master branch");
+
+
 printf("hello world");
   
   print("added this line, so it is different from the previous master branch");
